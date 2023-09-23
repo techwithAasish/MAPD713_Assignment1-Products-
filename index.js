2cf24c06900257dcb515a290ef4c76958af54341
techwithAasish <aasish.mahato101@gmail.com>
2023-09-23 19:04:36 -0400

    server created

---
+++ index.js
@@ -0,0 +1,22 @@
+let SERVER_NAME = "product-api";
+let PORT = 5000;
+let HOST = "127.0.0.1";
+
+let errors = require("restify-errors");
+let restify = require("restify"),
+  // Get a persistence engine for the users
+  productsSave = require("save")("products"),
+  // Create the restify server
+  server = restify.createServer({ name: SERVER_NAME });
+
+server.listen(PORT, HOST, function () {
+  console.log("Server %is listening at %s", server.name, server.url);
+  console.log("**** Resources: ****");
+  console.log("********************");
+  console.log("Endpoints:", server.url, +"method: GET, POST, DELETE");
+  console.log("/products");
+  console.log("/products/:id");
+});
+
+server.use(restify.plugins.fullResponse());
+server.use(restify.plugins.bodyParser());
